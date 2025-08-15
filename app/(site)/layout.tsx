1f8a590c2995105ed7f0e7a715f56cc95b68600f
Constantin Potapov <constantin@potapov.me>
2025-08-15 15:21:05 +0300

    добавил счетчик

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import {FaTelegramPlane, FaGithub, FaEnvelope, FaYoutube} from "react-icons/fa";
 import {SiHabr, SiVk} from "react-icons/si";
 import FooterConnectionsClient from "./_components/FooterConnectionsClient";
+import Script from "next/script";
 
 export default function RootLayout({
                                        children,
@@ -81,6 +82,31 @@ export default function RootLayout({
                 </footer>
             </div>
             <Toaster/>
+            {/* Yandex.Metrika counter */}
+            <Script id="yandex-metrika" strategy="afterInteractive">
+                {`
+                    (function(m,e,t,r,i,k,a){
+                        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103766300', 'ym');
+
+                    ym(103766300, 'init', {
+                        ssr: true,
+                        webvisor: true,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        ecommerce:"dataLayer"
+                    });
+                `}
+            </Script>
+            <noscript>
+                <div><img src="https://mc.yandex.ru/watch/103766300" style={{position: 'absolute', left: '-9999px'}}
+                          alt=""/></div>
+            </noscript>
+            {/* /Yandex.Metrika counter */}
         </>
     );
 }
